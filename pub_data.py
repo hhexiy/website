53113b47045aa5c5bf5a76f608490b050de08446
heherebecca <hehe.rebecca@gmail.com>
2016-04-06 17:04:57 -0700

    first commit

---
+++ pub_data.py
@@ -0,0 +1,175 @@
+pubs = []
+
+pubs.append({
+    'bib': 'he2016interpretese',
+    'title': 'Interpretese vs. Translationese: The Uniqueness of Human Strategies in Simultaneous Interpretation',
+    'author': ('me', 'jordan', 'hal'),
+    'year': '2016',
+    'venue': 'naacl',
+    'type': 'conference',
+    'pdf': 'papers/2016_naacl_interpretese.pdf',
+    'code': 'https://github.com/hhexiy/interpretese',
+  })
+
+pubs.append({
+    'bib': 'he2016aia',
+    'title': 'Active Information Acquisition',
+    'author': ('me', 'paul', 'nikos'),
+    'year': '2016',
+    'venue': 'arxiv',
+    'index': '1602.02181',
+    'type': 'arxiv',
+    'pdf': 'http://arxiv.org/abs/1602.02181',
+  })
+
+pubs.append({
+    'title': 'Interactive Incremental Question Answering',
+    'author': ('jordan', 'mohit', 'me', 'hal'),
+    'year': '2015',
+    'venue': 'nips',
+    'type': 'demo',
+    'award': ('Outstanding Demonstration Award', 'https://nips.cc/Conferences/2015/Awards'),
+  })
+
+pubs.append({
+    'bib': 'he2015rewrite',
+    'title': 'Syntax-based Rewriting for Simultaneous Machine Translation',
+    'author': ('me', 'alvin', 'jjm', 'jordan', 'hal'),
+    'year': '2015',
+    'venue': 'emnlp',
+    'type': 'conference',
+    'pdf': 'papers/2015_simtrans_rewrite.pdf',
+  })
+
+pubs.append({
+    'bib': 'chang2015dep',
+    'title': 'Learning to Search for Dependencies',
+    'author': ('kaiwei', 'me', 'hal', 'langford'),
+    'year': '2015',
+    'venue': 'arxiv',
+    'type': 'arxiv',
+    'index': '1503.05615',
+    'pdf': 'http://arxiv.org/abs/1503.05615',
+    'code': 'https://github.com/JohnLangford/vowpal_wabbit/wiki/Tutorial',
+  })
+
+pubs.append({
+    'bib': 'liu2015multitrust',
+    'title': 'Crowdsourcing with Multi-Dimensional Trust',
+    'author': ('xiangyang', 'me', 'baras'),
+    'year': '2015',
+    'venue': 'fusion',
+    'type': 'conference',
+    'award': ('Tammy L. Blair Award Runner-up', 'http://fusion2015.org/awards/')
+  })
+
+pubs.append({
+    'bib': 'liu2015budget',
+    'title': 'Trust-Aware Optimal Crowdsourcing With Budget Constraint',
+    'author': ('xiangyang', 'me', 'baras'),
+    'year': '2015',
+    'venue': 'icc',
+    'type': 'conference',
+  })
+
+pubs.append({
+    'bib': 'li2014temporal',
+    'title': 'Temporal Supervised Learning for Inferring a Dialog Policy from Example Conversations',
+    'author': ('lihong', 'me', 'jasonw'),
+    'year': '2014',
+    'venue': 'slt',
+    'type': 'conference',
+    'pdf': 'papers/paper-slt.pdf',
+    'poster': 'presentation/tsl-poster-3.pdf',
+  })
+
+pubs.append({
+    'bib': 'he2014bb',
+    'title': 'Learning to Search in Branch and Bound Algorithms',
+    'author': ('me', 'hal', 'eisner'),
+    'year': '2014',
+    'venue': 'nips',
+    'type': 'conference',
+    'pdf': 'papers/ilp-bb.pdf',
+    'poster': 'presentation/ilp-bb-poster-nips2014.pdf',
+    'code': 'https://github.com/hhexiy/scip-dagger',
+  })
+
+pubs.append({
+    'bib': 'grissom2014simtrans',
+    'title': "Don't Until the Final Verb Wait: Reinforcement Learning for Simultaneous Machine Translation",
+    'author': ('alvin', 'me', 'jjm', 'jordan', 'hal'),
+    'year': '2014',
+    'venue': 'emnlp',
+    'type': 'conference',
+    'pdf': 'papers/2014_emnlp_simtrans.pdf',
+    'talk': 'https://youtu.be/hVoxXO3F468',
+  })
+
+pubs.append({
+    'bib': 'he2013dep',
+    'title': 'Dynamic Feature Selection for Dependency Parsing',
+    'author': ('me', 'hal', 'eisner'),
+    'year': '2013',
+    'venue': 'emnlp',
+    'type': 'conference',
+    'pdf': 'papers/depFeat.pdf',
+    'slides': 'presentation/depFeat_slides.pdf',
+    'screencast': 'presentation/depFeat_screencast.mp5',
+  })
+
+pubs.append({
+    'bib': 'he2012coaching',
+    'title': 'Imitation Learning by Coaching',
+    'author': ('me', 'hal', 'eisner'),
+    'year': '2012',
+    'venue': 'nips',
+    'type': 'conference',
+    'pdf': 'papers/dynafea_im.pdf',
+    'poster': 'presentation/dynafea_im_poster.pdf',
+  })
+
+pubs.append({
+    'bib': 'boyd-graber2012qb',
+    'title': 'Besting the Quiz Master: Crowdsourcing Incremental Classification Games',
+    'author': ('jordan', 'brianna', 'me', 'hal'),
+    'year': '2012',
+    'venue': 'emnlp',
+    'type': 'conference',
+    'pdf': 'papers/qb.pdf'
+  })
+
+pubs.append({
+    'bib': 'he2012dynafea',
+    'title': 'Cost-sensitive dynamic feature selection',
+    'author': ('me', 'hal', 'eisner'),
+    'year': '2012',
+    'venue': 'icml',
+    'ws': 'inferning',
+    'type': 'workshop',
+    'pdf': 'papers/dynafea_ws.pdf',
+    'slides': 'presentation/dynafea_slides.pdf',
+    'poster': 'presentation/dynafea_poster.pdf',
+  })
+
+pubs.append({
+    'bib': 'he2011superres',
+    'title': 'Single Image Super-resolution using Gaussian Process Regression',
+    'author': ('me', 'wan-chi'),
+    'year': '2011',
+    'venue': 'cvpr',
+    'type': 'conference',
+    'pdf': 'papers/srgpr.pdf'
+  })
+
+pubs.append({
+    'bib': 'he2010rare',
+    'title': 'Rare Class classification with SVM',
+    'author': ('me', 'ali'),
+    'year': '2010',
+    'venue': 'icpr',
+    'type': 'conference',
+    'pdf': 'papers/rcsvm.pdf',
+    'code': 'code/rcsvm_matlab.zip',
+  })
+
---
+++ venue.py
@@ -0,0 +1,14 @@
+venue = {
+    'naacl': ('North American Chapter of the Association for Computational Linguistics', 'NAACL'),
+    'arxiv': ('arXiv', ''),
+    'emnlp': ('Empirical Methods in Natural Language Processing', 'EMNLP'),
+    'fusion': ('International Conference on Information Fusion', 'Fusion'),
+    'icc': ('International Conference on Communications', 'ICC'),
+    'slt': ('Spoken Lanugage Technology Workshop', 'SLT'),
+    'nips': ('Neural Information Processing Systems', 'NIPS'),
+    'cvpr': ('Computer Vision and Pattern Recognition', 'CVPR'),
+    'icpr': ('International Conference on Pattern Recognition', 'ICPR'),
+    'icml': ('International Conference on Machine Learning', 'ICML'),
+    # workshop
+    'inferning': ('Inferning', 'http://inferning.cs.umass.edu/2012'),
+    }
